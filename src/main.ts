8fa0f1d82fd072c5a445a150a90ff8ae6a95acf4
KHab1bull0 <habibullotoyrilmasov@gmail.com>
2024-07-09 02:24:03 +0500

    First commit

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { loggerFunction } from './middlewares/middleware';
 
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.use(loggerFunction);
 
-  await app.listen(4000);
+  await app.listen(4000), () => {
+    console.log("Server is working on port: ", 4000);
+  };
 }
 bootstrap();
+
